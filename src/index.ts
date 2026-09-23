a95344452f1c29121592b0fb7030074a06e82ba2
Ahmad Jaber <ahmadjaber340@hotmail.com>
2026-09-23 13:15:09 +0300

    update repo structure and audit fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-export * from "./components";
 export * from "./hooks";
 export * from "./lib";
-export * from "./api";
-export * from "./routes";
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
@@ -6,8 +7,16 @@ import { resolve } from "path";
 export default defineConfig({
   plugins: [
     react(),
-    dts({ include: ["src"], exclude: ["src/App.tsx", "src/main.tsx"] }),
+    dts({
+      include: ["src"],
+      exclude: ["src/App.tsx", "src/main.tsx", "src/**/*.test.{ts,tsx}"],
+    }),
   ],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./src/setupTests.ts"],
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
